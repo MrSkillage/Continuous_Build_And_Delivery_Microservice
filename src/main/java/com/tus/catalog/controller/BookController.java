c9474db20ed6d485fc933191e47093e6a09a12c8
MrSkillage <43910132+MrSkillage@users.noreply.github.com>
2023-04-14 16:37:22 +0100

    Webhook Testing

--- src/main/java/com/tus/catalog/controller/BookController.java
+++ src/main/java/com/tus/catalog/controller/BookController.java
@@ -26,10 +26,6 @@ public class BookController {
 
     @Autowired
     private BookService bookService;
-
-    public void newMethod() {
-    	System.out.println("A new method to Test the Webhook!");
-    }
     
     @GetMapping("")
     public ResponseEntity<List<Book>> getAllBooks() {
